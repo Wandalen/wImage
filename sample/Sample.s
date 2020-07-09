ab9e0066d15730c781ff9d922ed332392eb20392
wandalen <wandalen.me@gmail.com>
2020-07-09 09:01:01 +0300

    .

--- sample/Sample.s
+++ sample/Sample.s
@@ -3,5 +3,5 @@ let _ = require( 'wimage' );
 
 /**/
 
-let image = _.image.fileRead( __dirname + '/../proto/dwtools/amid/l3/image.test/_assets/basic/Pixels-2x2.png' ).structure;
+let image = _.image.fileRead( __dirname + '/../proto/wtools/amid/l3/image.test/_assets/basic/Pixels-2x2.png' ).structure;
 console.log( image );
