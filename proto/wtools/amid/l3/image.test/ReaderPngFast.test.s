6505613a2e9b79de04d0ea1b93f35873565b9d04
Jack Shell <jackshel45@gmail.com>
2020-08-04 20:05:05 +0300

    Added backend plugin fast-png

---
+++ proto/wtools/amid/l3/image.test/ReaderPngFast.test.s
@@ -0,0 +1,55 @@
+( function _ReaderPngFast_test_s_()
+{
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  let _ = require( '../../../../wtools/Tools.s' );
+  require( '../image/entry/ReaderPngFast.s' );
+  require( './ReaderAbstractPng.test.s' );
+}
+
+let _ = _global_.wTools;
+let Parent = _global_.wTests.ImageReadAbstractPng;
+
+// --
+// context
+// --
+
+// --
+// tests
+// --
+
+// --
+// declare
+// --
+
+var Proto =
+{
+
+  name : 'ImageReadPngFast',
+  abstract : 0,
+
+  context :
+  {
+    ext : 'png',
+    format : 'png',
+    readerName : 'PngFast',
+  },
+
+  tests :
+  {
+
+
+  },
+
+}
+
+//
+
+var Self = new wTestSuite( Proto ).inherit( Parent );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+})();
---
+++ proto/wtools/amid/l3/image/l5_reader/PngFast.s
@@ -0,0 +1,333 @@
+( function _PngFast_s_()
+{
+
+'use strict';
+
+/**
+ * @classdesc Abstract interface to read image.
+ * @class wImageReaderPngFast
+ * @namespace wTools
+ * @module Tools/mid/ImageReader
+ */
+
+let _ = _global_.wTools;
+let { decode } = require( 'fast-png' );
+let Backend = { decode };
+let bufferFromStream = require( './BufferFromStream.s' );
+let Parent = _.image.reader.Abstract;
+let Self = wImageReaderPngFast;
+function wImageReaderPngFast()
+{
+
+  return _.workpiece.construct( Self, this, arguments );
+}
+
+Self.shortName = 'PngFast';
+
+// --
+// implementation
+// --
+
+function _structureHandle( o )
+{
+  let self = this;
+  let os = o.originalStructure;
+  console.log( 'OS: ', os )
+  if( os === null )
+  os = o.op.originalStructure;
+
+  // logger.log( '_structureHandle', o.mode );
+  _.assertRoutineOptions( _structureHandle, arguments );
+  _.assert( _.objectIs( os ) );
+
+  if( o.mode === 'full' && o.op.mode === 'full' )
+  o.op.structure.buffer = _.bufferRawFrom( os.data );
+  else
+  o.op.structure.buffer = null;
+
+  if( o.op.headGot )
+  return o.op;
+
+  o.op.structure.dims = [ os.width, os.height ];
+
+  o.op.originalStructure = os;
+
+  if( os.channels === 3 )
+  {
+    _.assert( o.op.structure.channelsArray.length === 0 );
+    channelAdd( 'red' );
+    channelAdd( 'green' );
+    channelAdd( 'blue' );
+  }
+  else if( os.channels === 4 )
+  {
+    _.assert( o.op.structure.channelsArray.length === 0 );
+    channelAdd( 'red' );
+    channelAdd( 'green' );
+    channelAdd( 'blue' );
+    channelAdd( 'alpha' );
+  }
+
+  if( os.channels === 1 )
+  {
+    _.assert( o.op.structure.channelsArray.length === 0 );
+    channelAdd( 'gray' );
+  }
+  else if( os.channels === 2 )
+  {
+    _.assert( o.op.structure.channelsArray.length === 0 );
+    channelAdd( 'gray' );
+    channelAdd( 'alpha' );
+  }
+
+  o.op.structure.bitsPerPixel = os.depth;
+
+  // TEST: no info about interlacing
+  // o.op.structure.special.interlaced = false;
+  o.op.structure.hasPalette = os.palette !== undefined;
+
+  o.op.headGot = true;
+
+  if( o.op.onHead )
+  o.op.onHead( o.op );
+
+  return o.op;
+
+  /* */
+
+  function channelAdd( name )
+  {
+    o.op.structure.channelsArray.push( name );
+  }
+
+}
+
+_structureHandle.defaults =
+{
+  op : null,
+  originalStructure : null,
+  mode : null,
+}
+
+//
+
+function _read( o )
+{
+  let self = this;
+  _.assert( arguments.length === 1 );
+  _.assertRoutineOptions( _read, o );
+  o.mode = 'full';
+  return self._readGeneral( o );
+}
+
+_read.defaults =
+{
+  ... Parent.prototype._read.defaults,
+}
+
+//
+
+function _readHead ( o )
+{
+  let self = this;
+  _.assert( arguments.length === 1 );
+  _.assertRoutineOptions( _readHead, o );
+  o.mode = 'head';
+  return self._readGeneral( o );
+}
+
+_readHead.defaults =
+{
+  ... Parent.prototype._read.defaults,
+}
+
+//
+
+function _readGeneral( o )
+{
+  let self = this;
+
+  _.assertRoutineOptions( _readGeneral, o );
+  _.assert( arguments.length === 1 );
+  _.assert( _.longHas( [ 'full', 'head' ], o.mode ) );
+
+  o.headGot = false;
+
+  if( _.streamIs( o.data ) )
+  if( o.sync )
+  return self._readGeneralStreamSync( o )
+  else
+  return self._readGeneralStreamAsync( o );
+
+  if( o.sync )
+  return self._readGeneralBufferSync( o );
+  else
+  return self._readGeneralBufferAsync( o );
+
+}
+
+_readGeneral.defaults =
+{
+  ... Parent.prototype._read.defaults,
+  mode : 'full',
+}
+
+//
+
+function _readGeneralStreamAsync( o )
+{
+  let self = this;
+  let ready = bufferFromStream({ src : o.data });
+
+  ready.then( ( buffer ) =>
+  {
+    o.data = _.bufferNodeFrom( buffer );
+    return self._readGeneralBufferAsync( o );
+  } )
+
+  return ready;
+}
+
+//
+
+function _readGeneralStreamSync( o )
+{
+  let self = this;
+  let ready = self._readGeneralStreamAsync( o );
+  ready.deasync();
+  return ready.sync();
+}
+
+//
+
+function _readGeneralBufferSync( o )
+{
+  let self = this;
+  try
+  {
+    let image = Backend.decode( _.bufferNodeFrom( o.data ) );
+    self._structureHandle({ originalStructure : image, op : o, mode : 'full' });
+  }
+  catch( err )
+  {
+    throw _.err( err );
+  }
+  return o;
+}
+
+//
+
+function _readGeneralBufferAsync( o )
+{
+  let self = this;
+  let ready = new _.Consequence();
+  try
+  {
+    let image = Backend.decode( _.bufferNodeFrom( o.data ) );
+    self._structureHandle({ originalStructure : image, op : o, mode : 'full' });
+    ready.take( o );
+  }
+  catch( err )
+  {
+    throw _.err( err );
+  }
+  return ready;
+
+}
+
+
+// --
+// relations
+// --
+
+let Formats = [ 'png' ];
+let Exts = [ 'png' ];
+
+let Composes =
+{
+}
+
+let Aggregates =
+{
+}
+
+let Associates =
+{
+}
+
+let Restricts =
+{
+}
+
+let Statics =
+{
+  Formats,
+  Exts,
+  SupportsStream : 0,
+  SupportsAsync : 1,
+  SupportsSync : 1,
+  SupportsReadHead : 0
+}
+
+let Forbids =
+{
+}
+
+let Accessors =
+{
+}
+
+let Medials =
+{
+}
+
+// --
+// prototype
+// --
+
+let Extension =
+{
+  _structureHandle,
+  _readGeneralBufferSync,
+  _readGeneralBufferAsync,
+  _readGeneralStreamAsync,
+  _readGeneralStreamSync,
+  _readGeneral,
+
+  _readHead,
+  _read,
+
+  //
+
+  Formats,
+  Exts,
+
+  //
+
+  Composes,
+  Aggregates,
+  Associates,
+  Restricts,
+  Medials,
+  Statics,
+  Forbids,
+  Accessors,
+
+}
+
+//
+
+_.classDeclare
+({
+  cls : Self,
+  parent : Parent,
+  extend : Extension,
+});
+
+//
+
+_.image.reader[ Self.shortName ] = Self;
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = Self;
+
+})();
